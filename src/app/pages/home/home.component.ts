c2fb881653e6688765bcbe801d4b3087a54d485e
elene <eleneudzilauri@yahoo.com>
2023-09-30 20:44:25 +0400

    [up] functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROW_HEIGHT: { [id: number]: number} = { 1: 400, 3: 335, 4: 350 }
 
@@ -14,12 +16,23 @@ export class HomeComponent {
 
   category: string | undefined;
 
+  constructor(private cartService: CartService) {}
+
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
     this.rowHeight = ROW_HEIGHT[this.cols];
   };
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id,
+    })
   }
 
 
